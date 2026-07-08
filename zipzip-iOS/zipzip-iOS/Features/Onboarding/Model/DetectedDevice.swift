e6a0119ac6d81ce11a473b8a7262ac930ea54615
kyooonnnggg <gyoeunjung26@sookmyung.ac.kr>
2026-07-08 15:56:41 +0900

    feat: 온보딩 기기 선택 뷰 모델 분리

---
+++ zipzip-iOS/zipzip-iOS/Features/Onboarding/Model/DetectedDevice.swift
@@ -0,0 +1,66 @@
+//
+//  DetectedDevice.swift
+//  zipzip-iOS
+//
+//  Created by Codex on 7/8/26.
+//
+
+import SwiftUI
+
+struct DetectedDevice: Identifiable, Hashable {
+    let id: String
+    let name: String
+    let modelName: String
+    let type: DeviceType
+
+    init(
+        id: String? = nil,
+        name: String,
+        modelName: String,
+        type: DeviceType
+    ) {
+        self.id = id ?? "\(type)-\(name)-\(modelName)"
+        self.name = name
+        self.modelName = modelName
+        self.type = type
+    }
+}
+
+enum DeviceType: Hashable {
+    case camera
+    case phone
+
+    var icon: ImageResource {
+        switch self {
+        case .camera: .camera
+        case .phone: .iphone
+        }
+    }
+
+    var iconSize: CGSize {
+        switch self {
+        case .camera: CGSize(width: 33, height: 24)
+        case .phone: CGSize(width: 20, height: 32)
+        }
+    }
+}
+
+extension DetectedDevice {
+    static let samples: [DetectedDevice] = [
+        DetectedDevice(
+            name: "캐논 디지털 카메라",
+            modelName: "Canon IXUS 860",
+            type: .camera
+        ),
+        DetectedDevice(
+            name: "아이폰",
+            modelName: "iphone 6",
+            type: .phone
+        ),
+        DetectedDevice(
+            name: "아이폰",
+            modelName: "iphone 6s",
+            type: .phone
+        )
+    ]
+}
