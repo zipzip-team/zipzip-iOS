2025fa0871c89d50b0431f9dd08966f50c3cab78
kyooonnnggg <gyoeunjung26@sookmyung.ac.kr>
2026-07-08 15:51:53 +0900

    feat: router 설정

---
+++ zipzip-iOS/zipzip-iOS/App/RootView.swift
@@ -0,0 +1,35 @@
+//
+//  RootView.swift
+//  zipzip-iOS
+//
+//  Created by 성환 on 7/7/26.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    @Environment(Router.self) private var router
+
+    var body: some View {
+        @Bindable var router = router
+        NavigationStack(path: $router.path) {
+            SplashView()
+                .navigationDestination(for: Route.self) { route in
+                    switch route {
+                    case .splash:
+                        SplashView()
+                    case .serviceIntro:
+                        ServiceIntroView()
+                    case .onboardingComplete:
+                        OnboardingCompleteView()
+                    case .photoPermission:
+                        PhotoPermissionView()
+                    case .deviceLoading:
+                        DeviceLoadingView()
+                    case .deviceSelection:
+                        DeviceSelectionView()
+                    }
+                }
+        }
+    }
+}
