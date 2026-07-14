c80024871e75c04ae62678fb4a936410a5bd3da7
kyooonnnggg <gyoeunjung26@sookmyung.ac.kr>
2026-07-15 07:17:27 +0900

    fix: 등록 기기 목록만 스크롤되도록 수정

--- zipzip-iOS/zipzip-iOS/Features/MyPage/Component/MyPageContainerView.swift
+++ zipzip-iOS/zipzip-iOS/Features/MyPage/Component/MyPageContainerView.swift
@@ -11,6 +11,7 @@ struct MyPageContainerView<TopBar: View, Content: View>: View {
     private let topPadding: CGFloat
     private let horizontalPadding: CGFloat
     private let alignment: Alignment
+    private let isContentScrollable: Bool
     private let topBar: TopBar
     private let content: Content
 
@@ -18,20 +19,29 @@ struct MyPageContainerView<TopBar: View, Content: View>: View {
         topPadding: CGFloat = 0,
         horizontalPadding: CGFloat = 0,
         alignment: Alignment = .top,
+        isContentScrollable: Bool = true,
         @ViewBuilder topBar: () -> TopBar,
         @ViewBuilder content: () -> Content
     ) {
         self.topPadding = topPadding
         self.horizontalPadding = horizontalPadding
         self.alignment = alignment
+        self.isContentScrollable = isContentScrollable
         self.topBar = topBar()
         self.content = content()
     }
 
     var body: some View {
-        ScrollView {
-            content
-                .padding(.top, FloatingHeaderLayout.buttonHeight + 30)
+        Group {
+            if isContentScrollable {
+                ScrollView {
+                    content
+                        .padding(.top, FloatingHeaderLayout.buttonHeight + 30)
+                }
+            } else {
+                content
+                    .padding(.top, FloatingHeaderLayout.buttonHeight + 30)
+            }
         }
         .padding(.top, topPadding)
         .padding(.horizontal, horizontalPadding)
