d6b3a30a3f0d6795a66136e7f4a937f9543732dc
kyooonnnggg <gyoeunjung26@sookmyung.ac.kr>
2026-07-15 07:41:11 +0900

    fix: 네비바 탭 전환 시 명시적 스프링 애니메이션 삭제

--- zipzip-iOS/zipzip-iOS/Shared/Component/Navbar/Navbar.swift
+++ zipzip-iOS/zipzip-iOS/Shared/Component/Navbar/Navbar.swift
@@ -26,9 +26,7 @@ struct Navbar: View {
     private func item(for tab: NavbarTab) -> some View {
         let isSelected = selection == tab
         return Button {
-            withAnimation(.spring(response: 0.3, dampingFraction: 0.8)) {
-                onSelect(tab)
-            }
+            onSelect(tab)
         } label: {
             EmptyView()
         }
